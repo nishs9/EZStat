c0c66a6def806fc5758403731191a405056d83a7
nishs9 <snishk@gmail.com>
2019-09-15 20:31:36 -0400

    beginning to replace PySimpleGUI with PyQt

--- gui.py
+++ gui.py
@@ -1,3 +1,20 @@
-import PySimpleGUI as sg
-import GUIStack as stack
-import pageLayout as pL
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+
+qtCreatorFile = "UI Designs/homepage.ui"
+
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+class EZStat(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        self.submitButton.clicked.connect(##choose a functions)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    homepage = EZStat()
+    homepage.show()
+    sys.exit(app.exec_())
--- testapi.py
+++ testapi.py
@@ -2,43 +2,50 @@ import base64
 import requests
 import json
 import csv
+from ohmysportsfeedspy import MySportsFeeds
 
-try:
-    response = requests.get(
-        url='https://api.mysportsfeeds.com/v2.1/pull/nfl/latest/standings.json',
-        params={
-            "fordate": "20190723"
-        },
-        headers={
-            "Authorization": "Basic " + base64.b64encode('{}:{}'.format('83298d15-107d-4444-a888-3523ba','MYSPORTSFEEDS').encode('utf-8')).decode('ascii')
-        }
-    )
-    print('Response HTTP Status Code: {status_code}'.format(
-        status_code=response.status_code))
-    # print('Response HTTP Response Body: {content}'.format(
-    #     content=response.content))
-    # jsonResp = response.json()
-    # print(jsonResp['teams'])
-except requests.exceptions.RequestException:
-    print('HTTP Request failed')
+msf = MySportsFeeds(version = "2.0")
+msf.authenticate("83298d15-107d-4444-a888-3523ba", "MYSPORTSFEEDS")
 
-jsonResp = response.json()
-print(jsonResp)
-teamList = jsonResp['teams']
+response = msf.msf_get_data(league='nfl',season='2018-2019-regular',feed='seasonal_player_stats',format='json',player='matt-ryan')
+print(response)
 
-with open('standings.csv', mode = 'w', newline = '') as file:
-	csvFile = csv.writer(file, delimiter = ',')
-	csvFile.writerow(['Team','W','L','T','Win %', 'GB'])
-	nfcWestDict = {}
-	for i in range(32):
-		newTeam = []
-		if teamList[i]['divisionRank']['divisionName'] == 'NFC West':
-			newTeam.append(teamList[i]['team']['city'] + " " + teamList[i]['team']['name'])
-			newTeam.append(teamList[i]['stats']['standings']['wins'])
-			newTeam.append(teamList[i]['stats']['standings']['losses'])
-			newTeam.append(teamList[i]['stats']['standings']['ties'])
-			newTeam.append(teamList[i]['stats']['standings']['winPct'])
-			newTeam.append(teamList[i]['divisionRank']['gamesBack'])
-			nfcWestDict[teamList[i]['divisionRank']['rank']] = newTeam
-	for j in range(1,5):
-		csvFile.writerow(nfcWestDict[j])
+
+
+# try:
+#     response = requests.get(
+#         url='https://api.mysportsfeeds.com/v2.1/pull/nfl/2018-2019-regular/player_stats_totals.json',
+#         params={
+#             'player':'Matt Ryan'
+#         },
+#         headers={
+#             "Authorization": "Basic " + base64.b64encode('{}:{}'.format('83298d15-107d-4444-a888-3523ba','MYSPORTSFEEDS').encode('utf-8')).decode('ascii')
+#         }
+#     )
+#     print('Response HTTP Status Code: {status_code}'.format(
+#         status_code=response.status_code))
+# except requests.exceptions.RequestException:
+#     print('HTTP Request failed')
+#
+# jsonResp = response.json()
+# parsed = json.dumps(jsonResp)
+# parsed2 = json.loads(parsed)
+# print(json.dumps(parsed2, indent = 4, sort_keys=False))
+# teamList = jsonResp['teams']
+#
+# with open('standings.csv', mode = 'w', newline = '') as file:
+# 	csvFile = csv.writer(file, delimiter = ',')
+# 	csvFile.writerow(['Team','W','L','T','Win %', 'GB'])
+# 	nfcWestDict = {}
+# 	for i in range(32):
+# 		newTeam = []
+# 		if teamList[i]['divisionRank']['divisionName'] == 'NFC West':
+# 			newTeam.append(teamList[i]['team']['city'] + " " + teamList[i]['team']['name'])
+# 			newTeam.append(teamList[i]['stats']['standings']['wins'])
+# 			newTeam.append(teamList[i]['stats']['standings']['losses'])
+# 			newTeam.append(teamList[i]['stats']['standings']['ties'])
+# 			newTeam.append(teamList[i]['stats']['standings']['winPct'])
+# 			newTeam.append(teamList[i]['divisionRank']['gamesBack'])
+# 			nfcWestDict[teamList[i]['divisionRank']['rank']] = newTeam
+# 	for j in range(1,5):
+# 		csvFile.writerow(nfcWestDict[j])
